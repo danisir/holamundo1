00286ba7e89c45b6a0ab65c357617739c0e309d6
inkaris <ionekaitz2@hotmail.com>
2015-03-16 11:58:56 +0100

    Update holamundo1.java

--- src/holamundo/holamundo1.java
+++ src/holamundo/holamundo1.java
@@ -3,7 +3,7 @@ package holamundo;
 public class holamundo1 {
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
-		System.out.println("-----2");
+		System.out.println("-----323232");
 		System.out.println("hola mundo bis");
 	}
 
