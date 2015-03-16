07e9119bd364166803badb2bbb97dcb6ca14bd34
danisir <daniel30>
2015-03-16 11:51:02 +0100

    bis

--- src/holamundo/holamundo1.java
+++ src/holamundo/holamundo1.java
@@ -3,7 +3,7 @@ package holamundo;
 public class holamundo1 {
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
-		System.out.println("-------");
+		System.out.println("-----25");
 		System.out.println("hola mundo bis");
 	}
 
